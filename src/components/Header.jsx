352ca244fd9a870c29b570241962b313a3b902b9
Natalie <natalie.ann.kwak@gmail.com>
2025-12-17 19:10:23 -0500

    home page done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
-import { useState } from "react";
-import { Button, ButtonGroup, Drawer, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  Drawer,
+  DrawerContent,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  useDisclosure,
+} from "@heroui/react";
 import HomeIcon from "@mui/icons-material/Home";
 import MenuIcon from "@mui/icons-material/Menu";
 import StickyNote2Icon from "@mui/icons-material/StickyNote2";
@@ -8,66 +17,59 @@ import WorkIcon from "@mui/icons-material/Work";
 import logo from "../assets/stick_head.PNG";
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
+  const handleOpen = () => {
+    onOpen();
   };
 
   return (
-    <Stack
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      sx={{ width: "100%", maxHeight: 80, padding: 2 }}
-    >
-      <Button
-        onClick={toggleDrawer(true)}
-        sx={{ position: "absolute", left: 16, color: "black" }}
+    <Navbar className="max-h-20 p-2 shadow-sm">
+      <NavbarBrand className="">
+        <Button
+          isIconOnly
+          variant="light"
+          onPress={() => handleOpen()}
+        >
+          <MenuIcon fontSize="large" />
+        </Button>
+      </NavbarBrand>
+      <Drawer
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="left"
+        className="max-w-62.5"
       >
-        <MenuIcon fontSize="large" />
-      </Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        <Stack direction="column" sx={{ width: 250, padding: 2 }}>
-          <ButtonGroup
-            orientation="vertical"
-            aria-label="Site Menu"
-            variant="text"
-            color="black"
-            sx={{ width: "100%" }}
-          >
-            <Button
-              startIcon={<HomeIcon />}
-              color="black"
-              size="large"
-              sx={{ justifyContent: "flex-start" }}
-            >
+        <DrawerContent>
+          <ButtonGroup>
+            <Button startIcon={<HomeIcon />} size="large">
               Home
             </Button>
             <Button
               startIcon={<WorkIcon />}
-              color="black"
-              size="large"
               sx={{ justifyContent: "flex-start" }}
             >
               Portfolio
             </Button>
             <Button
               startIcon={<StickyNote2Icon />}
-              color="black"
-              size="large"
               sx={{ justifyContent: "flex-start" }}
             >
               Resume
             </Button>
           </ButtonGroup>
-        </Stack>
+        </DrawerContent>
       </Drawer>
-      <img src={logo} alt="Logo" style={{ height: 40, marginRight: 16 }} />
-      <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-        Hayden Lyle
-      </Typography>
-    </Stack>
+
+      <NavbarContent>
+        <NavbarItem>
+          <img src={logo} alt="Logo" className="h-10" />
+        </NavbarItem>
+        <NavbarItem>
+          <h1 className="font-extrabold text-3xl">Hayden Lyle</h1>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,24 @@
-import { Grid, Typography } from "@mui/material";
-
 import { Header } from "../components";
+import figure from "../assets/stick_guy.PNG";
 
 const HomePage = () => {
   return (
-    <Grid
-      container
-      sx={{ width: "100%", maxWidth: "full", margin: 0, padding: 0 }}
-    >
+    <>
       <Header />
-      <Typography variant="h2">howdy</Typography>
-    </Grid>
+      <div className="flex flex-row h-full w-full justify-center items-center mt-20 p-10">
+        <img src={figure} alt="Stick Figure" className="h-70 shrink-0" />
+        <div className="flex flex-col max-w-xl mr-8">
+          <h1 className="text-4xl mb-2">Hi, I'm Hayden!</h1>
+          <p className="wrap-break-word">
+            Placeholder text for an 'About Me' blurb. Placeholder text for an
+            'About Me' blurb. Placeholder text for an 'About Me' blurb.
+            Placeholder text for an 'About Me' blurb. Placeholder text for an
+            'About Me' blurb. Placeholder text for an 'About Me' blurb.
+            Placeholder text for an 'About Me' blurb.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
